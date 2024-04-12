c93f30a49cb89aef8d97aa398c7e0c1b8b5902d4
Suman Kumar Sah <suman.kumar@whatfix.com>
2024-04-12 08:01:53 +0530

    inital commit

---
+++ otp-login/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the first project</h1>
+      <h2>This is working </h2>
+    </div>
+  );
+}
+
+export default App;
